9ff0c48103af0557ddf9383aeddb94a8becbfbfe
Janik Dotzel <janik.dotzel@lightbend.com>
2022-06-28 18:46:15 +0200

    add grpc descriptors

--- shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
+++ shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
@@ -8,10 +8,10 @@ import akka.grpc.GrpcServiceException
 import akka.util.Timeout
 import io.grpc.Status
 import org.slf4j.LoggerFactory
-import shopping.cart.proto.{Cart, CheckoutRequest}
+import shopping.cart.proto.{ Cart, CheckoutRequest, GetCartRequest }
 
 class ShoppingCartServiceImpl(system: ActorSystem[_])
-  extends proto.ShoppingCartService {
+    extends proto.ShoppingCartService {
   import system.executionContext
 
   private val logger = LoggerFactory.getLogger(getClass)
@@ -26,7 +26,7 @@ class ShoppingCartServiceImpl(system: ActorSystem[_])
     logger.info("addItem {} to cart {}", in.itemId, in.cartId)
     val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
     val reply: Future[ShoppingCart.Summary] =
-      entityRef.askWithStatus( ShoppingCart.AddItem(in.itemId, in.quantity, _))
+      entityRef.askWithStatus(ShoppingCart.AddItem(in.itemId, in.quantity, _))
     val response = reply.map(cart => toProtoCart(cart))
     convertError(response)
   }
@@ -35,14 +35,16 @@ class ShoppingCartServiceImpl(system: ActorSystem[_])
     logger.info(s"checkout cart ${in.cartId}")
     val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
     val reply = entityRef.askWithStatus(ref => ShoppingCart.Checkout(ref))
-    val response = reply.map( cart => toProtoCart(cart))
+    val response = reply.map(cart => toProtoCart(cart))
     convertError(response)
   }
 
   private def toProtoCart(cart: ShoppingCart.Summary): proto.Cart = {
-    proto.Cart(cart.items.iterator.map { case (itemId, quantity) =>
-      proto.Item(itemId, quantity)
-    }.toSeq)
+    proto.Cart(
+      cart.items.iterator.map { case (itemId, quantity) =>
+        proto.Item(itemId, quantity)
+      }.toSeq,
+      cart.checkedOut)
   }
 
   private def convertError[T](response: Future[T]): Future[T] = {
@@ -58,4 +60,17 @@ class ShoppingCartServiceImpl(system: ActorSystem[_])
     }
   }
 
+  override def getCart(in: proto.GetCartRequest): Future[proto.Cart] = {
+    logger.info("getCart {}", in.cartId)
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val response =
+      entityRef.ask(ShoppingCart.Get).map { cart =>
+        if (cart.items.isEmpty)
+          throw new GrpcServiceException(
+            Status.NOT_FOUND.withDescription(s"Cart ${in.cartId} not found"))
+        else
+          toProtoCart(cart)
+      }
+    convertError(response)
+  }
 }
